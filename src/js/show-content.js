228def61983f04917dfb3f2964caddbe186b0a6f
Mayya <khmelnitskaya@wiseadvice.ru>
2020-06-17 21:53:31 +0300

    предварительная разметка стилизация блока с инструментами

---
+++ src/js/show-content.js
@@ -0,0 +1,31 @@
+var ShowContent = {
+  el: '.js-show-content',
+  name: 'ShowContent',
+
+  initialize: function () {
+    this.btn = this.$('.js-show-content__btn');
+    this.hiddenContent = this.$('.js-show-content__content');
+    var self = this;
+  },
+
+  events: {
+    'click .js-show-content__btn': 'showContent'
+  },
+
+
+  showContent: function (e) {
+    e.preventDefault();
+
+    $(e.currentTarget).next('.js-show-content__content').slideToggle();
+
+    // Если скрытую информацию и кнопку-триггер невозможно разместить в общем контейнере
+    // Или скрытая информация расположена не после кнопки-триггера
+    if (this.$el.filter('[data-attribute]')) {
+      this.dataTarget = $(e.currentTarget).attr('data-attribute');
+      this.$el.find($('[data-target=' + this.dataTarget + ']')).slideToggle();
+    }
+
+  }
+};
+
+App.Control.install(ShowContent);
