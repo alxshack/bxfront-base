8ef13d6547e88fecfb0c510052a6476107c0611d
Mayya <khmelnitskaya@wiseadvice.ru>
2020-06-25 11:59:31 +0300

    доп стили

--- src/js/logos-slider.js
+++ src/js/logos-slider.js
@@ -1,27 +1,27 @@
-var LogosSlider = {
-  el: '.js-logos-slider',
-  name: 'LogosSlider',
-
-  initialize: function () {
-    this.$el.slick({
-      dots: false,
-      arrows:true,
-      infinite: true,
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 6,
-            arrows: false,
-            variableWidth:true,
+(function ($) {
+  App.Widgets = App.Widgets || {};
+  App.Widgets.LogosSlider = can.Control.extend({
+    pluginName: 'appWidgetLogosSlider'
+  }, {
+    init: function () {
+      this.$el.slick({
+        dots: false,
+        arrows:true,
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 6,
+              arrows: false,
+              variableWidth:true,
+            }
           }
-        }
-      ]
-    });
+        ]
+      });
+    }
+  });
+}(jQuery));
 
-  }
-};
-
-App.Control.install(LogosSlider);
