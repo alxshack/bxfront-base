6b4f63581c9abae86b1e0a7b0370c19c1a3f485e
shack-al <tihomirov@wiseadvice.ru>
2020-06-26 10:05:22 +0300

    перенастройка сборщика на использование CanJS и набора контроллеров от проекта 1cwa, итерация 03

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -5,7 +5,9 @@ var _ = require('underscore');
 _.gulp.task('scripts:core:dist', function () {
 
 	return _.gulp.src([
-		'node_modules/jquery/dist/jquery.min.js',
+		// 'node_modules/jquery/dist/jquery.min.js',
+		'bower_components/jquery/dist/jquery.js',
+		'bower_components/jquery-migrate/jquery-migrate.js',
 		'node_modules/underscore/underscore-min.js',
 		'bower_components/CanJS/can.jquery.min.js',
 		'bower_components/CanJS/can.construct.super.js',
@@ -15,6 +17,15 @@ _.gulp.task('scripts:core:dist', function () {
 		'bower_components/select2/dist/js/select2.min.js',
 		'bower_components/tooltipster/dist/js/tooltipster.bundle.min.js',
 		'bower_components/fancybox/source/jquery.fancybox.pack.js',
+		'bower_components/uri.js/src/URI.js',
+		'bower_components/uri.js/src/URI.fragmentQuery.js',
+		'bower_components/js-cookie/src/js.cookie.js',
+		'bower_components/owl.carousel/dist/owl.carousel.js',
+		'bower_components/iCheck/icheck.js',
+		'bower_components/jquery.maskedinput/dist/jquery.maskedinput.js',
+		'bower_components/modernizr/modernizr.js',
+		'bower_components/magnific-popup/dist/jquery.magnific-popup.js',
+
 		_.gulp.localSourcePath + '/js_core/*.js'
 	])
 		.pipe(_.$.concat('core.min.js'))
